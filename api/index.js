5b57bd034d537ec1a4d181bb35e2db8e85d1efeb
kashish <kashishchadha0509@gmail.com>
2025-06-22 13:58:44 +0530

    added test api

--- api/index.js
+++ api/index.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js';
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  console.log('Connected to MongoDB');
+  }).catch(err => {
+  console.error('Error connecting to MongoDB:', err);
+});
 const app=express();
 const port=3000;
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+
+app.use('/', userRoutes);  
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
